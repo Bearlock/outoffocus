89d55d2a05fc6d780f361aea8749445f822ad6b2
Bearlock <ediaz@bamboohr.com>
2019-02-28 08:49:38 -0700

    Add iTunes badge

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 import { Link, graphql } from "gatsby"
 import logo from "../images/logo.jpg"
+import apple from "../images/apple-podcasts-badge.png"
 
 export default ({ data }) => {
   const latest = data.allMarkdownRemark.edges[0].node;
@@ -31,6 +32,7 @@ export default ({ data }) => {
         <div css={css`text-align: center`}>
           <div css={css`display: inline-block`}>
             <a href='https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&amp;isi=691797987&amp;ius=googleplaymusic&amp;apn=com.google.android.music&amp;link=https://play.google.com/music/m/Ipxkorsy5uzsfjd2ylcjcosxlxu?t%3DOut_of_Focus%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16' rel='nofollow'><img width='125px' alt='Listen on Google Play Music' src='https://play.google.com/intl/en_us/badges-music/images/badges/en_badge_web_music.png'/></a>
+            <a href='https://itunes.apple.com/us/podcast/out-of-focus/id1454423231?mt=2&ls=1'><img css={ css`width: 125px; height: 45.875px; margin-left: 20px;` } alt="Listen on Apple iTunes" src={ apple } /></a>
           </div>
         </div>
       </div>
